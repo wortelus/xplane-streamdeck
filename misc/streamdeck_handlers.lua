e76d3efaa20f2f7565bffac7be29ac21e56011fd
wortelus <daniel.slavik.cz@gmail.com>
2022-06-29 15:48:50 +0200

    added LUA handler for momentary switches in X-Plane

---
+++ misc/streamdeck_handlers.lua
@@ -0,0 +1,20 @@
+create_command( "FlyWithLua/streamdeck_handler/fire_test_right_start", "fire test zibo right start handler", "command_begin(\"laminar/B738/toggle_switch/fire_test_rgt\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/fire_test_right_end", "fire test zibo right end handler", "command_end(\"laminar/B738/toggle_switch/fire_test_rgt\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/fire_test_left_start", "fire test zibo left start handler", "command_begin(\"laminar/B738/toggle_switch/fire_test_lft\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/fire_test_left_end", "fire test zibo left end handler", "command_end(\"laminar/B738/toggle_switch/fire_test_lft\")", "", "" )
+
+create_command( "FlyWithLua/streamdeck_handler/capt_fire_warn_start", "Capt fire warn light start handler", "command_begin(\"laminar/B738/push_button/fire_bell_light1\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/capt_fire_warn_end", "Capt fire warn light end handler", "command_end(\"laminar/B738/push_button/fire_bell_light1\")", "", "" )
+
+create_command( "FlyWithLua/streamdeck_handler/capt_master_caution_start", "Capt master caution light start handler", "command_begin(\"laminar/B738/push_button/master_caution1\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/capt_master_caution_end", "Capt master caution light end handler", "command_end(\"laminar/B738/push_button/master_caution1\")", "", "" )
+
+create_command( "FlyWithLua/streamdeck_handler/capt_six_pack_start", "Capt six pack start handler", "command_begin(\"laminar/B738/push_button/capt_six_pack\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/capt_six_pack_end", "Capt six pack end handler", "command_end(\"laminar/B738/push_button/capt_six_pack\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/fo_six_pack_start", "F/O six pack start handler", "command_begin(\"laminar/B738/push_button/capt_six_pack\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/fo_six_pack_end", "F/O six pack end handler", "command_end(\"laminar/B738/push_button/capt_six_pack\")", "", "" )
+
+create_command( "FlyWithLua/streamdeck_handler/flt_alt_down_start", "flt alt down start handler", "command_begin(\"laminar/B738/knob/flt_alt_press_dn\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/flt_alt_down_end", "flt alt down end handler", "command_end(\"laminar/B738/knob/flt_alt_press_dn\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/flt_alt_up_start", "flt alt up start handler", "command_begin(\"laminar/B738/knob/flt_alt_press_up\")", "", "" )
+create_command( "FlyWithLua/streamdeck_handler/flt_alt_up_end", "flt alt up end handler", "command_end(\"laminar/B738/knob/flt_alt_press_up\")", "", "" )
