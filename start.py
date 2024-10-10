5af166c3090fed739df9fc06515fc294551704ca
wortelus <daniel.slavik.cz@gmail.com>
2024-10-10 22:57:01 +0200

    added debug start argument

--- start.py
+++ start.py
@@ -1,19 +1,42 @@
+import argparse
 import logging as logger
 import sys
 
 from xpsd import control
+
+""" Main entry point for xplane-streamdeck by wortelus. """
+
+formatter = logger.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 root = logger.getLogger()
 root.setLevel(logger.INFO)
 
 handler = logger.StreamHandler(sys.stdout)
 handler.setLevel(logger.INFO)
-formatter = logger.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
+
 root.addHandler(handler)
 
 
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description='Run xplane-streamdeck by wortelus.')
+    parser.add_argument('--debug', action='store_true', help='Enable debug output')
+    return parser.parse_args()
+
 def main():
+    args = parse_args()
+
+    # Set log level based on debug flag
+    if args.debug:
+        root.setLevel(logger.DEBUG)
+        handler.setLevel(logger.DEBUG)
+        logger.debug("Debugging is enabled.")
+    else:
+        root.setLevel(logger.INFO)
+        handler.setLevel(logger.INFO)
+
+
     logger.info("Starting xplane-streamdeck by wortelus. This software is licensed under BSD 2-Clause License.")
     logger.info("Copyright (c) 2022, Daniel Slavík All rights reserved.")
     conf = control.load()
